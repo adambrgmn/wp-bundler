076eb2c01afd60cf1e9ead7be7c19b129e6ea1df
Adam Bergman <adam@fransvilhelm.com>
2021-09-29 10:27:13 +0200

    Tweak dev bundler output

--- bundle.mjs
+++ bundle.mjs
@@ -5,6 +5,7 @@ const args = process.argv.slice(2);
 
 (async () => {
   const pkg = JSON.parse(await fs.readFile('./package.json', 'utf-8'));
+  const watch = args.includes('--watch') || args.includes('-w');
   try {
     await build({
       entryPoints: ['./src/index.ts'],
@@ -14,19 +15,24 @@ const args = process.argv.slice(2);
       platform: 'node',
       target: 'node14',
       external: Object.keys(pkg.dependencies),
-      watch:
-        args.includes('--watch') || args.includes('-w')
-          ? {
-              onRebuild(error) {
-                if (error == null) {
-                  console.log('Rebuild succeeded.');
-                } else {
-                  console.error('Rebuild failed.');
-                }
-              },
-            }
-          : false,
+      watch: watch
+        ? {
+            onRebuild(error) {
+              if (error == null) {
+                console.log('Rebuild succeeded.');
+              } else {
+                console.error('Rebuild failed.');
+              }
+            },
+          }
+        : false,
     });
+
+    if (watch) {
+      console.log('Initial build done.');
+    } else {
+      console.log('Build completed.');
+    }
   } catch (error) {
     console.error(error);
     process.exit(1);
