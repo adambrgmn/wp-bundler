72753e5ecfb50726d19bb20fad428f11123927d5
Adam Bergman <adam.bergman@klarna.com>
2022-07-06 14:06:59 +0200

    Use yargs to improve cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,40 +1,104 @@
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 import { Bundler } from './bundler';
 import { Runner } from './runner';
 import { Server } from './server';
-import { CliArgsSchema } from './schema';
-import { parseArgv } from './utils/parse-argv';
-import pkg from '../package.json';
+import { Mode } from './types';
 
-export async function cli(argv: typeof process.argv) {
-  let args = parseArgv({
-    argv,
-    schema: CliArgsSchema,
-    alias: { mode: 'm', watch: 'w', help: 'h', version: 'v' },
-  });
+export async function cli() {
+  let argv = await yargs(hideBin(process.argv))
+    .command(
+      'build',
+      'Create production ready version of your project',
+      {
+        mode: {
+          alias: 'm',
+          default: 'prod',
+          description: 'Version of your source to output (prod or dev)',
+        },
+        cwd: {
+          required: false,
+          description: 'Optional path to your project',
+          type: 'string',
+        },
+      },
+      (argv) => {
+        let cwd = argv.cwd ?? process.cwd();
+        let mode = ensureMode(argv.mode, 'prod');
+        let port = 3000;
+        let host = 'localhost';
 
-  if (args.version) {
-    console.log(pkg.version);
-    return process.exit(0);
+        let bundler = new Bundler({ mode, cwd, host, port });
+        let server = new Server({ port, host, cwd });
+        let runner = new Runner({ bundler, server, cwd });
+
+        runner.build();
+      },
+    )
+    .command(
+      'dev',
+      'Run a development server',
+      {
+        host: {
+          alias: 'h',
+          default: 'localhost',
+          description: 'Host to bind the server to',
+        },
+        port: {
+          alias: 'p',
+          default: 3000,
+          description: 'Port to bind the server to',
+        },
+        mode: {
+          alias: 'm',
+          default: 'dev',
+          description: 'Version of your source to output (prod or dev)',
+        },
+        cwd: {
+          required: false,
+          description: 'Optional path to your project',
+          type: 'string',
+        },
+      },
+      (argv) => {
+        let cwd = argv.cwd ?? process.cwd();
+        let mode = ensureMode(argv.mode, 'dev');
+        let port = argv.port;
+        let host = argv.host;
+
+        let bundler = new Bundler({ mode, cwd, host, port });
+        let server = new Server({ port, host, cwd });
+        let runner = new Runner({ bundler, server, cwd });
+
+        runner.watch();
+      },
+    )
+    .parse();
+
+  if (argv._.length === 0) {
+    console.warn(
+      'Using wp-bundler without a sub command is deprecated and will be removed in the next major release.\n' +
+        'Instead you can use `wp-bundler build` or `wp-bundler dev`.\n\n' +
+        'See wp-bundler --help for more information.',
+    );
+
+    let cwd = typeof argv.cwd === 'string' ? argv.cwd : process.cwd();
+    let mode = ensureMode(argv.mode, argv.watch ? 'dev' : 'prod');
+    let port = Number(argv.port) ?? 3000;
+    let host = typeof argv.host === 'string' ? argv.host : 'localhost';
+
+    let bundler = new Bundler({ mode, cwd, host, port });
+    let server = new Server({ port, host, cwd });
+    let runner = new Runner({ bundler, server, cwd });
+
+    if (argv.watch) {
+      runner.watch();
+    } else {
+      runner.build();
+    }
   }
+}
 
-  if (args.help) {
-    console.log('help');
-    return process.exit(0);
-  }
-
-  const cwd = args.cwd ?? process.cwd();
-  const watch = args.watch ?? false;
-  const mode = args.mode ?? watch ? 'dev' : 'prod';
-  const port = args.port ?? 3000;
-  const host = args.host ?? 'localhost';
-
-  let bundler = new Bundler({ mode, cwd, host, port });
-  let server = new Server({ port, host, cwd });
-  let runner = new Runner({ bundler, server, cwd });
-
-  if (watch) {
-    await runner.watch();
-  } else {
-    await runner.build();
-  }
+function ensureMode(value: unknown, fallback: Mode): Mode {
+  return typeof value === 'string' && ['dev', 'prod'].includes(value) ? (value as Mode) : fallback;
 }
