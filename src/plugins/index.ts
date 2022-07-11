c5676552c35202993a173578b00d3e5c4b7adb79
Adam Bergman <adam.bergman@klarna.com>
2022-07-11 16:12:40 +0200

    Remove unused php plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,6 @@
 export * from './define';
 export * from './externals';
-export * from './php';
+// export * from './php';
 export * from './postcss';
 export * from './swc';
 export * from './translations';
