9b67c56915805adf58b289fc8ec54f3e18ec0527
Adam Bergman <adam.bergman@klarna.com>
2022-07-12 15:53:58 +0200

    Remove old example folder

--- example/src/utils/math.ts
+++
@@ -1,7 +0,0 @@
-export async function add(...nums: number[]) {
-  return nums.reduce((a, b) => a + b, 0);
-}
-
-export async function sub(init: number, ...nums: number[]) {
-  return nums.reduce((a, b) => a - b, init);
-}
