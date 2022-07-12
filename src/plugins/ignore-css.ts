4a7648ae7fc4e29696fe9015e107341b5ab56074
Adam Bergman <adam.bergman@klarna.com>
2022-07-12 09:38:39 +0200

    Skip emitting confusing nomodule css

---
+++ src/plugins/ignore-css.ts
@@ -0,0 +1,33 @@
+import { BuildOptions, Plugin } from 'esbuild';
+
+import { BundlerPlugin, BundlerPluginOptions } from '../types';
+
+export const ignoreCss: BundlerPlugin = () => {
+  return {
+    name: 'wp-bundler-ignore-css',
+    setup(build) {
+      build.initialOptions.entryPoints = removeCssEntrypoints(build.initialOptions.entryPoints);
+
+      build.onResolve({ filter: /\.css$/, namespace: 'file' }, async () => {
+        return { external: true };
+      });
+    },
+  };
+};
+
+function removeCssEntrypoints(entryPoints: BuildOptions['entryPoints']) {
+  if (entryPoints == null) return undefined;
+
+  if (Array.isArray(entryPoints)) {
+    return entryPoints.filter((entryPoint) => !entryPoint.endsWith('.css'));
+  }
+
+  let newEntryPoints: Record<string, string> = {};
+  for (let [key, value] of Object.entries(entryPoints)) {
+    if (!value.endsWith('.css')) {
+      newEntryPoints[key] = value;
+    }
+  }
+
+  return newEntryPoints;
+}
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,6 @@
 export * from './define';
 export * from './externals';
-// export * from './php';
+export * from './ignore-css';
 export * from './postcss';
 export * from './swc';
 export * from './translations';
