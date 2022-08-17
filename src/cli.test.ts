27d25adcb6807df1d371d5489a4e26b082ed2791
Adam Bergman <adam.bergman@klarna.com>
2022-08-17 13:22:13 +0200

    Update snapshots

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -30,12 +30,12 @@ describe('Theme', () => {
     expect(read.theme()).toMatchInlineSnapshot(`
       Array [
         "AssetLoader.php",
-        "admin.DB333OT5.js",
-        "admin.FO3PWPK4.nomodule.js",
+        "admin.BRZ25XL2.nomodule.js",
+        "admin.KRT7TWFX.js",
         "languages",
-        "main.6OVZDUF3.js",
+        "main.A4RZX4NB.nomodule.js",
         "main.TGLZWPB2.css",
-        "main.VXFGVGGY.nomodule.js",
+        "main.UNJITHPP.js",
       ]
     `);
   });
@@ -48,10 +48,10 @@ describe('Plugin', () => {
     expect(read.plugin()).toMatchInlineSnapshot(`
       Array [
         "AssetLoader.php",
-        "admin.IMWKQWPT.js",
-        "admin.YXSFU7UU.nomodule.js",
-        "main.24T3P6PJ.js",
-        "main.AXIEJDQJ.nomodule.js",
+        "admin.B2QIUYTG.nomodule.js",
+        "admin.EYKEWSYL.js",
+        "main.DPDP3RVU.nomodule.js",
+        "main.ZY3XGKPK.js",
       ]
     `);
   });
@@ -61,7 +61,23 @@ describe('Build', () => {
   it('bundles js/ts in dev mode without transpilation', async () => {
     await $.plugin('build', ['--mode', 'dev']);
     expect(read.plugin('admin.js')).toMatchInlineSnapshot(`
-      "// src/log.ts
+      "var __getOwnPropNames = Object.getOwnPropertyNames;
+      var __esm = (fn, res) => function __init() {
+        return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])(fn = 0)), res;
+      };
+
+      // ../../assets/wp-element/wp-element.ts
+      var init_wp_element = __esm({
+        \\"../../assets/wp-element/wp-element.ts\\"() {
+          \\"use strict\\";
+        }
+      });
+
+      // src/admin.ts
+      init_wp_element();
+
+      // src/log.ts
+      init_wp_element();
       var log = (...messages2) => {
         console.log(...messages2);
       };
@@ -79,7 +95,7 @@ describe('Build', () => {
     let items = read.plugin();
     let admin = items.find((item) => item.endsWith('.js') && item.startsWith('admin')) as string;
     expect(read.plugin(admin)).toMatchInlineSnapshot(`
-      "var o=(...l)=>{console.log(...l)};var e=[\\"Hello\\",\\"World\\"];o(...e);
+      "\\"use strict\\";(function(){var f=function(r,e){return function(){return r&&(e=r(r=0)),e}};var o=f(function(){\\"use strict\\"});o();o();function a(r,e){(e==null||e>r.length)&&(e=r.length);for(var t=0,n=new Array(e);t<e;t++)n[t]=r[t];return n}function y(r){if(Array.isArray(r))return a(r)}function c(r){if(typeof Symbol!=\\"undefined\\"&&r[Symbol.iterator]!=null||r[\\"@@iterator\\"]!=null)return Array.from(r)}function s(){throw new TypeError(\\"Invalid attempt to spread non-iterable instance.\\\\\\\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\\")}function m(r){return y(r)||c(r)||p(r)||s()}function p(r,e){if(!!r){if(typeof r==\\"string\\")return a(r,e);var t=Object.prototype.toString.call(r).slice(8,-1);if(t===\\"Object\\"&&r.constructor&&(t=r.constructor.name),t===\\"Map\\"||t===\\"Set\\")return Array.from(t);if(t===\\"Arguments\\"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t))return a(r,e)}}var l=function(){for(var r=arguments.length,e=new Array(r),t=0;t<r;t++)e[t]=arguments[t];var n;(n=console).log.apply(n,m(e))};function i(r,e){(e==null||e>r.length)&&(e=r.length);for(var t=0,n=new Array(e);t<e;t++)n[t]=r[t];return n}function b(r){if(Array.isArray(r))return i(r)}function A(r){if(typeof Symbol!=\\"undefined\\"&&r[Symbol.iterator]!=null||r[\\"@@iterator\\"]!=null)return Array.from(r)}function d(){throw new TypeError(\\"Invalid attempt to spread non-iterable instance.\\\\\\\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\\")}function g(r){return b(r)||A(r)||_(r)||d()}function _(r,e){if(!!r){if(typeof r==\\"string\\")return i(r,e);var t=Object.prototype.toString.call(r).slice(8,-1);if(t===\\"Object\\"&&r.constructor&&(t=r.constructor.name),t===\\"Map\\"||t===\\"Set\\")return Array.from(t);if(t===\\"Arguments\\"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t))return i(r,e)}}var h=[\\"Hello\\",\\"World\\"];l.apply(void 0,g(h));})();
       "
     `);
   });
@@ -89,7 +105,7 @@ describe('Build', () => {
     let items = read.plugin();
     let nomodule = items.find((item) => item.endsWith('.nomodule.js') && item.startsWith('admin')) as string;
     expect(read.plugin(nomodule)).toMatchInlineSnapshot(`
-      "\\"use strict\\";(function(){function o(r,n){(n==null||n>r.length)&&(n=r.length);for(var t=0,e=new Array(n);t<n;t++)e[t]=r[t];return e}function i(r){if(Array.isArray(r))return o(r)}function l(r){if(typeof Symbol!=\\"undefined\\"&&r[Symbol.iterator]!=null||r[\\"@@iterator\\"]!=null)return Array.from(r)}function f(){throw new TypeError(\\"Invalid attempt to spread non-iterable instance.\\\\\\\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\\")}function y(r){return i(r)||l(r)||c(r)||f()}function c(r,n){if(!!r){if(typeof r==\\"string\\")return o(r,n);var t=Object.prototype.toString.call(r).slice(8,-1);if(t===\\"Object\\"&&r.constructor&&(t=r.constructor.name),t===\\"Map\\"||t===\\"Set\\")return Array.from(t);if(t===\\"Arguments\\"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t))return o(r,n)}}var u=function(){for(var r=arguments.length,n=new Array(r),t=0;t<r;t++)n[t]=arguments[t];var e;(e=console).log.apply(e,y(n))};function a(r,n){(n==null||n>r.length)&&(n=r.length);for(var t=0,e=new Array(n);t<n;t++)e[t]=r[t];return e}function s(r){if(Array.isArray(r))return a(r)}function p(r){if(typeof Symbol!=\\"undefined\\"&&r[Symbol.iterator]!=null||r[\\"@@iterator\\"]!=null)return Array.from(r)}function b(){throw new TypeError(\\"Invalid attempt to spread non-iterable instance.\\\\\\\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\\")}function m(r){return s(r)||p(r)||A(r)||b()}function A(r,n){if(!!r){if(typeof r==\\"string\\")return a(r,n);var t=Object.prototype.toString.call(r).slice(8,-1);if(t===\\"Object\\"&&r.constructor&&(t=r.constructor.name),t===\\"Map\\"||t===\\"Set\\")return Array.from(t);if(t===\\"Arguments\\"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t))return a(r,n)}}var d=[\\"Hello\\",\\"World\\"];u.apply(void 0,m(d));})();
+      "\\"use strict\\";(function(){var f=function(r,e){return function(){return r&&(e=r(r=0)),e}};var o=f(function(){\\"use strict\\"});o();o();function a(r,e){(e==null||e>r.length)&&(e=r.length);for(var t=0,n=new Array(e);t<e;t++)n[t]=r[t];return n}function y(r){if(Array.isArray(r))return a(r)}function c(r){if(typeof Symbol!=\\"undefined\\"&&r[Symbol.iterator]!=null||r[\\"@@iterator\\"]!=null)return Array.from(r)}function s(){throw new TypeError(\\"Invalid attempt to spread non-iterable instance.\\\\\\\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\\")}function m(r){return y(r)||c(r)||p(r)||s()}function p(r,e){if(!!r){if(typeof r==\\"string\\")return a(r,e);var t=Object.prototype.toString.call(r).slice(8,-1);if(t===\\"Object\\"&&r.constructor&&(t=r.constructor.name),t===\\"Map\\"||t===\\"Set\\")return Array.from(t);if(t===\\"Arguments\\"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t))return a(r,e)}}var l=function(){for(var r=arguments.length,e=new Array(r),t=0;t<r;t++)e[t]=arguments[t];var n;(n=console).log.apply(n,m(e))};function i(r,e){(e==null||e>r.length)&&(e=r.length);for(var t=0,n=new Array(e);t<e;t++)n[t]=r[t];return n}function b(r){if(Array.isArray(r))return i(r)}function A(r){if(typeof Symbol!=\\"undefined\\"&&r[Symbol.iterator]!=null||r[\\"@@iterator\\"]!=null)return Array.from(r)}function d(){throw new TypeError(\\"Invalid attempt to spread non-iterable instance.\\\\\\\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\\")}function g(r){return b(r)||A(r)||_(r)||d()}function _(r,e){if(!!r){if(typeof r==\\"string\\")return i(r,e);var t=Object.prototype.toString.call(r).slice(8,-1);if(t===\\"Object\\"&&r.constructor&&(t=r.constructor.name),t===\\"Map\\"||t===\\"Set\\")return Array.from(t);if(t===\\"Arguments\\"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t))return i(r,e)}}var h=[\\"Hello\\",\\"World\\"];l.apply(void 0,g(h));})();
       "
     `);
   });
