9066ef7ce27b75d711c845a5bfdc491a724cb103
Adam Bergman <adam@fransvilhelm.com>
2022-08-25 11:35:10 +0200

    Add explicit exports from package

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export * from './bundler';
 export * from './cli';
+
+export { createRunner, createContext } from './runner';
+
+export { Bundler } from './bundler';
+export { Logger } from './logger';
+export { Server } from './server';
+export { Watcher } from './watcher';
+export { Writer } from './writer';
+
+export type { BundlerOptions, Mode, ProjectPaths, ProjectInfo } from './types';
